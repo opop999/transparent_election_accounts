f4d455c343e51e9ee47c0e5139ebbae9d8c2c5eb
Ondrej Pekacek <ondrej.pekacek@fsv.cuni.cz>
2021-07-01 15:31:05 +0200

    changing tidyverse to smaller number of loaded libraries to speed up the script

--- fio_scraping_script.R
+++ fio_scraping_script.R
@@ -1,5 +1,6 @@
 library(rvest)
-library(tidyverse)
+library(dplyr)
+library(readr)
 
 # SCRAPING FIO ÚČTŮ
 
@@ -73,7 +74,7 @@ scrape_fio_summary <- function(parties_accounts_links, parties_names) {
 
 ## Vstupy pro funkce FIO
 
-dir_name <- "test" # Specifikace nazvu slozky, kam budou csv soubory ulozeny
+dir_name <- "data_full" # Specifikace nazvu slozky, kam budou csv soubory ulozeny
 
 names <- c("pirati_stan", 
            "ods_kdu_top09",
