25a8f35d7043afb7de3133c0e7d8b9bb41ba1431
Ondrej Pekacek <ondrej.pekacek@fsv.cuni.cz>
2021-07-28 13:00:35 +0200

    Fix missing variable function input, Change output directory for individual datasets

--- 01_scrape_fio_accounts.R
+++ 01_scrape_fio_accounts.R
@@ -19,13 +19,20 @@ invisible(lapply(packages, library, character.only = TRUE))
 # Argument "parties_accounts_links" accepts vector with url links to FIO accounts
 # "parties_names" accepts parties' names - order must match "parties_accounts_links"
 
-scrape_fio <- function(accounts) {
+scrape_fio <- function(accounts, dir_name) {
   # We initialize empty dataset to which we add rows with each loop iteration
   merged_dataset <- tibble()
   
   # We have to create a desired directory, if one does not yet exist
   if (!dir.exists(dir_name)) {
     dir.create(dir_name)
+  } else {
+    print("Output directory already exists")
+  }
+  
+  # Repeat the check for subdirectory for individual account datasets
+  if (!dir.exists(paste0(dir_name, "/individual_accounts"))) {
+    dir.create(paste0(dir_name, "/individual_accounts"))
   } else {
     print("Output directory already exists")
   }
@@ -52,7 +59,7 @@ scrape_fio <- function(accounts) {
         amount = as.numeric(str_replace_all(string = amount, pattern = "(\\s+|[a-zA-Z])", replacement = ""))
       )
     
-    myfile <- paste0(dir_name, "/", accounts[[1]][i], ".csv")
+    myfile <- paste0(dir_name, "/individual_accounts/", accounts[[1]][i], ".csv")
     write_excel_csv(table_transactions, file = myfile)
     
     # With each iteration of loop, we append the complete dataset
@@ -73,7 +80,7 @@ scrape_fio <- function(accounts) {
 
 # 3. Function scrapes selected accounts summaries and saves them into one file
 
-scrape_fio_summary <- function(accounts) {
+scrape_fio_summary <- function(accounts, dir_name) {
   summary_list <- list()
   
   if (!dir.exists(dir_name)) {
@@ -120,6 +127,6 @@ accounts <- readRDS("data/accounts_fio.rds")
 
 ## 5. Running both of the functions
 
-scrape_fio(accounts = accounts)
+scrape_fio(accounts = accounts, dir_name = dir_name)
 
-scrape_fio_summary(accounts = accounts)
+scrape_fio_summary(accounts = accounts, dir_name = dir_name)
