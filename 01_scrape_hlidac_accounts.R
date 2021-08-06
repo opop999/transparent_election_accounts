db77e1fd853ef38fc85de4cc73297a1cd73a21ad
Ondrej Pekacek <ondrej.pekacek@fsv.cuni.cz>
2021-08-06 19:51:11 +0200

    Init function that scrapes Hlidac Statu bank account database

--- 01_scrape_hlidac_accounts.R
+++ 01_scrape_hlidac_accounts.R
@@ -1,9 +1,14 @@
+
+# WORK IN PROGRESS --------------------------------------------------------
+
+
+
 # SCRAPING OF BANK ACCOUNTS FROM HLIDAC STATU DATABASE
 
 ## 1. Loading the required R libraries
 
 # Package names
-packages <- c("rvest", "dplyr", "data.table", "arrow", "stringr")
+packages <- c("httr", "data.table", "arrow", "dplyr", "jsonlite")
 
 # Install packages not yet installed
 installed_packages <- packages %in% rownames(installed.packages())
@@ -15,10 +20,134 @@ if (any(installed_packages == FALSE)) {
 invisible(lapply(packages, library, character.only = TRUE))
 
 
+# Extracting information for donation accounts
+#  ANO
+# -https://www.kb.cz/cs/transparentni-ucty/4070217
+# 
+# KSČM 
+# -https://transparentniucty.moneta.cz/homepage/-/transparent-account/7777377773
+# -https://www.csob.cz/portal/podnikatele-firmy-a-instituce/produkty/ucty-a-platebni-styk/bezne-platebni-ucty/transparentni-ucet/ucet/-/tu/280728599
+# 
+# TOP09
+# -https://www.csas.cz/cs/transparentni-ucty#/000000-0020091122
 
 
+donation_accounts_yesterday <- function(account, account_name, start_date, end_date, dir_name, sort, descending) {
+  
+  # We initialize empty dataset to which we add rows with each loop iteration
+  yesterday_data <- tibble()
+  
+  # We have to create a desired directory, if one does not yet exist
+  if (!dir.exists(dir_name)) {
+    dir.create(dir_name)
+  } else {
+    print("Output directory already exists")
+  }
+  
+  # Construct url call to query the number of pages from yesterday's posts
+  url_call <- paste0(
+    "https://www.hlidacstatu.cz/api/v2/datasety/transparentni-ucty-transakce/hledat?dotaz=CisloUctu%3A",
+    account,
+    "%20Datum%3A%5B",
+    start_date,
+    "%20TO%20",
+    end_date,
+    "%5D&sort=",
+    sort,
+    "&desc=",
+    descending
+  )
+  
+  # Get total number of pages from the initial result. We divide the number of
+  # total posts by 25 (this is the size of one API page) and apply ceiling to get
+  # full number
+  pages <- ceiling(
+    fromJSON(
+      content(
+        GET(
+          url_call,
+          add_headers(
+            "Authorization" = Sys.getenv("HS_TOKEN")
+          )
+        ),
+        as = "text"
+      )
+    )[[1]] / 25
+  )
+  
+  # Unfortunately, Hlidac's API supports an upper limit of 200 pages so we have to
+  # set an upper hard limit
+  for (i in seq_len(pages)[seq_len(pages) <= 200]) {
+    
+    # Formulate URL for the GET request for Facebook from yesterday
+    paginated_url_call <- paste0(url_call, "&strana=", i)
+    
+    # Send GET request to the API of Hlidac Statu
+    result_raw <- GET(paginated_url_call, add_headers("Authorization" = Sys.getenv("HS_TOKEN")))
+    
+    # Transform JSON output to a dataframe
+    result_df <- fromJSON(content(result_raw, as = "text"))[[3]]
+    
+    yesterday_data <- bind_rows(yesterday_data, result_df)
+  }
+  
+  # Only append the full dataset if there are records from yesterday
+  if (account_name == "Facebook" & !dim(yesterday_data)[1] == 0) {
+    # We are saving the merged dataframes with yesterday's data as CSV
+    fwrite(x = yesterday_data, file = paste0(dir_name, "/individual_donation_accounts/yesterday_data_donation_", tolower(account_name), ".csv"))
+
+    # Load in the existing full dataset merge with yesterday's new data
+    all_data <- readRDS(paste0(dir_name, "/all_data_", tolower(account_name), ".rds"))
+    
+    # Append the existing dataset with new rows from yesterday and delete duplicates
+    all_data <- bind_rows(yesterday_data, all_data) %>% distinct()
+    
+    # Save full dataset again both in CSV, RDS and also Arrow/Feather binary format
+    saveRDS(object = all_data, file = paste0(dir_name, "/all_data_", tolower(account_name), ".rds"), compress = FALSE)
+    fwrite(x = all_data, file = paste0(dir_name, "/all_data_", tolower(account_name), ".csv"))
+    write_feather(x = all_data, sink = paste0(dir_name, "/all_data_", tolower(account_name), ".feather"))
+    
+  } else if (account_name == "Facebook" & dim(yesterday_data)[1] == 0) {
+    print("FB dataset from yesterday is empty, no need to append")
+  }
+  
+}
+
+## 3. Inputs for the function
+dir_name <- "data/donation_accounts/" # Specify the folder, where the tables will be saved
+
+start_date <- Sys.Date() - 1000 # We select yesterday's date in YYYY-MM-DD format
+
+end_date <- Sys.Date() # Same as start_date - we only want yesterday
+
+sort <- "Datum" # Which column is used for sorting? We keep this consistent
+
+descending <- 1 # 1 is descending sort, 0 is ascending. We keep this consistent
+
+# account <- "20091122/0800"
+# account_name <- "top_09_cs"
+# 
+# account <- "280728599/0300"
+# account_name <- "kscm_csob"
+
+# account <- "7777377773/0600"
+# account_name <- "kscm_moneta"
+
+# account <- "4070217/0100"
+# account_name <- "ano_kb"
+
+# account <- "20091122/0800"
+# account_name <- "top_09_cs"
+# 
+# account <- "217343303/0300"
+# account_name <- "kscm_csob_spend"
+
+# 
+#  account <- "4090453/0100"
+#  account_name <- "ano_kb_spend"
 
 
+## 4. Running the function
 
 
 
